586352df19cd0590260e2f638ea81c8f4d161c29
Rocky Tseng <rocky.tseng@kla-tencor.com>
2023-07-12 02:03:28 -0700

    Minor update

--- red-black-tree/compile.sh
+++ red-black-tree/compile.sh
@@ -4,3 +4,4 @@ gcc -g -Wall  main.c -o main.o -c
 gcc -g -Wall  rotation.c -o rotation.o -c
 gcc -g -Wall  insertion.c -o insertion.o -c
 gcc -g -o a.out main.o rotation.o insertion.o
+rm *.o vgcore.* log.core.*

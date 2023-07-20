a53fa0b04c7ee38319c94e5395e8730347f5dde0
Rocky Tseng <rocky.tseng@kla-tencor.com>
2023-07-20 02:43:07 -0700

    Minor update

--- self-balancing-interval-tree/macro.h
+++ self-balancing-interval-tree/macro.h
@@ -3,10 +3,10 @@
 #define LOCATION __FILE__, __FUNCTION__, __LINE__
 #define REPORT_ERROR { printf( "Error: %s(%s):%d\n", LOCATION ); exit(EXIT_FAILURE); }
 #define PRINT_LOCATION printf("%s(%s):%d\n", LOCATION);
-#define ROOT        0
-#define LEFT_CHILD  1
-#define RIGHT_CHILD 2
-#define NEEL        3
+#define ROOT        ( 1<<0 )
+#define LEFT_CHILD  ( 1<<1 )
+#define RIGHT_CHILD ( 1<<2 )
+#define NEEL        ( 1<<3 )
 #define RED   true
 #define BLACK false
 #define LOW_END  0
@@ -17,3 +17,4 @@
 
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
+
